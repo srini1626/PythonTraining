10f263b2c500612256a2c4df5aab74ecaafa2e21
srini1626 <srini1626@gmail.com>
2020-05-16 16:15:34 -0500

    List Python

---
+++ Python List-Array-Methods.py
@@ -0,0 +1,9 @@
+## Python List/Array Methods
+# 1.  Python List append() Method
+# Add an element to the fruits list:
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+fruits.append("orange")
+print(fruits)
+
